f038ff6e1354d6b44033c57b2941436d3b077f3e
Ivan Chen <ivanchenschool@gmail.com>
2017-01-02 22:43:10 -0500

    OK it draws cylinders now

--- OpenGLPractice/main.cpp
+++ OpenGLPractice/main.cpp
@@ -1,3 +1,4 @@
+
 //Include GLEW  
 #include <GL/glew.h>  
   
@@ -20,7 +21,168 @@ using namespace glm;
 #include "tree.h"
 #include <ctime>
 
+#include <math.h>
+#include <GL/glut.h>
+
 GLFWwindow* window;
+
+int drawTrees();
+void glTest();
+
+
+float gwindow;
+float t = 0.0f;
+
+
+void init (void) 
+{
+/*  select clearing (background) color       */
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+
+/*  initialize viewing values  */
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+
+void renderCylinder(float x1, float y1, float z1, float x2,float y2, float z2, float radius,int subdivisions,GLUquadricObj *quadric)
+{
+   float vx = x2-x1;
+   float vy = y2-y1;
+   float vz = z2-z1;
+
+   //handle the degenerate case of z1 == z2 with an approximation
+   if(vz == 0)
+       vz = .00000001;
+
+   float v = sqrt( vx*vx + vy*vy + vz*vz );
+   float ax = 57.2957795*acos( vz/v );
+   if ( vz < 0.0 )
+       ax = -ax;
+   float rx = -vy*vz;
+   float ry = vx*vz;
+   glPushMatrix();
+
+   //draw the cylinder body
+   glTranslatef( x1,y1,z1 );
+   glRotatef(ax, rx, ry, 0.0);
+   gluQuadricOrientation(quadric,GLU_OUTSIDE);
+   gluCylinder(quadric, radius, radius, v, subdivisions, 1);
+
+   //draw the first cap
+   gluQuadricOrientation(quadric,GLU_INSIDE);
+   gluDisk( quadric, 0.0, radius, subdivisions, 1);
+   glTranslatef( 0,0,v );
+
+   //draw the second cap
+   gluQuadricOrientation(quadric,GLU_OUTSIDE);
+   gluDisk( quadric, 0.0, radius, subdivisions, 1);
+   glPopMatrix();
+}
+
+void renderCylinder_convenient(float x1, float y1, float z1, float x2,float y2, float z2, float radius,int subdivisions)
+{
+   //the same quadric can be re-used for drawing many cylinders
+   GLUquadricObj *quadric=gluNewQuadric();
+   gluQuadricNormals(quadric, GLU_SMOOTH);
+   renderCylinder(x1,y1,z1,x2,y2,z2,radius,subdivisions,quadric);
+   gluDeleteQuadric(quadric);
+}
+
+void drawSolidCylinder(vec4 point1, vec4 point2, float radius, int divisions) {
+    GLUquadricObj *quadric = gluNewQuadric();
+    gluQuadricNormals(quadric, GLU_SMOOTH);
+   
+    float xDist = point2.x-point1.x;
+    float yDist = point2.y-point1.y;
+    float zDist = point2.z-point1.z;
+    float dist = sqrt(xDist*xDist + yDist*yDist + zDist*zDist);
+
+    float radToDeg = 57.2957795;
+    float ax = radToDeg * acos(zDist/dist);
+    if (zDist < 0.0)
+        ax = -ax;
+    float rx = -yDist*zDist;
+    float ry =  xDist*zDist;
+    
+    glPushMatrix();
+
+    //draw the cylinder body
+    glTranslatef(point1.x, point1.y, point1.z);
+    glRotatef(ax, rx, ry, 0.0);
+    gluQuadricOrientation(quadric, GLU_OUTSIDE);
+    gluCylinder(quadric, radius, radius, dist, divisions, 1);
+
+    //draw the first cap
+    gluQuadricOrientation(quadric, GLU_INSIDE);
+    gluDisk(quadric, 0.0, radius, divisions, 1);
+    glTranslatef(0, 0, dist);
+
+    //draw the second cap
+    gluQuadricOrientation(quadric, GLU_OUTSIDE);
+    gluDisk(quadric, 0.0, radius, divisions, 1);
+    glPopMatrix();
+
+    gluDeleteQuadric(quadric);
+}
+
+void display() {
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+   glLoadIdentity();
+   glTranslatef(0,0,-7);
+
+   drawSolidCylinder(vec4(0, -4, 0, 0), vec4(1, -2, 1, 0), 1, 20);
+   
+   t=0.53;
+   glutSwapBuffers();
+} 
+
+void InitGL(int Width, int Height) {
+   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+   glClearDepth(3.0);
+   glDepthFunc(GL_LESS);
+   glEnable(GL_DEPTH_TEST);
+
+   glShadeModel(GL_SMOOTH);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);
+   glMatrixMode(GL_MODELVIEW);
+
+   glEnable(GL_LIGHTING);
+   GLfloat LightAmbient[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+   GLfloat LightDiffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+   GLfloat LightPosition[] = { 0.0f, 0.0f, 2.0f, 1.0f };
+   glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);
+   glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);
+   glLightfv(GL_LIGHT1, GL_POSITION, LightPosition);
+   glEnable(GL_LIGHT1);
+}
+
+
+int glTest(int argc, char** argv) {
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+   glutInitWindowSize(640, 480);
+   gwindow = glutCreateWindow("");
+   glutDisplayFunc(&display);
+   //glutKeyboardFunc(&keyPressed);
+   //glutFullScreen();
+   glutIdleFunc(&display);
+   InitGL(640, 480);
+   glutMainLoop();
+   return 0;
+}
+
+
+
+int main(int argc, char** argv) {
+    //drawTrees();
+    glTest(argc, argv);
+}
+
 
 void startGlfw() {
 	// Initialise GLFW
@@ -53,8 +215,7 @@ void startGlfw() {
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 }
 
-
-int main( void ) {
+int drawTrees () {
     srand(time(0));
     startGlfw();
 
@@ -93,6 +254,14 @@ int main( void ) {
 
     
     Tree tree(branch, 6);
+
+
+    vec4 a(0, 0, 0, 0);
+    vec4 b(3, 3, -3, 0);
+    int radius = 10;
+    int pieces = 10;
+
+    vec4 dir = normalize(b-a);
 
     vector<float> vertices = tree.getVertices();
     const int size = tree.getSize();
@@ -151,4 +320,3 @@ int main( void ) {
 
 	return 0;
 }
-
